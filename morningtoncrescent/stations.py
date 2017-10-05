8f18819028af352dcdbd1d200ab56f46b3149c90
Daniel Pope <mauve@mauveweb.co.uk>
2017-10-05 21:01:04 +0100

    Load lines for each station

--- morningtoncrescent/stations.py
+++ morningtoncrescent/stations.py
@@ -5,9 +5,9 @@ import pkgutil
 from collections import namedtuple
 from io import StringIO
 import random
+from collections import defaultdict
 
-
-Station = namedtuple('Station', 'name zones postcode')
+Station = namedtuple('Station', 'name zones lines postcode')
 
 
 def postcode_zone(postcode):
@@ -17,15 +17,31 @@ def postcode_zone(postcode):
 def load_csv(name):
     stations = pkgutil.get_data(__name__, f'data/{name}.csv').decode('utf8')
     reader = csv.DictReader(StringIO(stations))
-    for row in reader:
+    yield from reader
+
+
+def load_stations():
+    lines = load_lines()
+    for row in load_csv('stations'):
         yield Station(
             name=row['Station'],
             zones={int(z) for z in row['Zone'].split(',') if z},
+            lines=lines[row['Station']],
             postcode=postcode_zone(row['Postcode']),
         )
 
 
-STATIONS = {s.name: s for s in load_csv('stations')}
+def load_lines():
+    by_station = defaultdict(set)
+    for row in load_csv('lines'):
+        line = row['Tube Line']
+        for k in ('From Station', 'To Station'):
+            station = row[k]
+            by_station[station].add(line)
+    return by_station
+
+
+STATIONS = {s.name: s for s in load_stations()}
 STATIONS_BY_ZONE = {}
 
 for s in STATIONS.values():
